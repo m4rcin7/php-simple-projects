0242c3eadc5420a29bb75dac10707192e719d08b
Marcin Kopatys <mrku1337@gmail.com>
2025-09-15 11:19:31 +0200

    register-function

--- login-system/register.php
+++ login-system/register.php
@@ -1,3 +1,59 @@
+<?php
+
+require_once "config.php";
+require_once "session.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+    $fullname = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+    $confirm_password = trim($_POST['confirm_password']);
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+    if ($query = $db->prepare("SELECT * FROM users WHERE email = ?")) {
+        
+        $error = '';
+        $query->bind_param('s', $email);
+        $query->execute();
+        $query->store_result();
+        
+        if ($query->num_rows > 0) {
+            $error .= '<p class="error">The email address is already registered!</p>';
+        } else {
+            if (strlen($password) < 6) {
+                $error .= '<p class="error">Password must have at least 6 characters.</p>';
+            }
+
+            if (empty($confirm_password)) {
+                $error .= '<p class="error">Please enter confirm password.</p>';
+            } else {
+                if (empty($error) && ($password != $confirm_password)) {
+                    $error .= '<p class="error">Passwords did not match.</p>';
+                }
+            }
+
+            if (empty($error)) {
+                if ($insertQuery = $db->prepare("INSERT INTO users (name, email, password) VALUES (?,?,?);")) {
+                    $insertQuery->bind_param("sss", $fullname, $email, $password_hash);
+                    $result = $insertQuery->execute();
+                    if ($result) {
+                        $error .= '<p class="success">Your registration was successful! :)</p>';
+                    } else {
+                        $error .= '<p class="error">Something went wrong!</p>';
+                    }
+                    $insertQuery->close();
+                }
+            }
+        }
+        $query->close();
+    }
+
+    mysqli_close($db);
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -81,6 +137,14 @@
             color: #4338ca;
         }
 
+        .error {
+            color: #ff6347;
+        }
+
+        .success {
+            color: #3cb371;
+        }
+ 
         @media (max-width: 480px) {
             form {
                 padding: 1.5rem;
